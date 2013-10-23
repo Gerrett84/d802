e723fc335ff59bcc6e18f957e43c60923b7bef3e
Gerrett84 <Gerrett84@gmail.com>
2013-10-23 18:41:17 +0200

    update sources

--- d802.mk
+++ d802.mk
@@ -27,7 +27,7 @@ PRODUCT_DEVICE := d802
 PRODUCT_NAME := full_d802
 PRODUCT_BRAND := LGE
 PRODUCT_MODEL := LG-D802
-PRODUCT_MANUFACTURER := LGE
+PRODUCT_MANUFACTURER := lge
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
 # Inherit from hardware-specific part of the product configuration
